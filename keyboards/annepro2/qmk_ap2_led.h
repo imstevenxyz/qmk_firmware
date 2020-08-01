80a27530bee9c01060f19a179611b0d998114c3b
Steven Impens <s.impens.dev@gmail.com>
2020-08-01 23:05:17 +0200

    Fix odd behaviour during boot

--- keyboards/annepro2/qmk_ap2_led.h
+++ keyboards/annepro2/qmk_ap2_led.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define CMD_LED_ON                          0x1
-#define CMD_LED_OFF                         0x2
-#define CMD_LED_SET                         0x3
-#define CMD_LED_SET_ROW                     0x4
+#define CMD_LED_ON                          0x10
+#define CMD_LED_OFF                         0x20
+#define CMD_LED_SET                         0x30
+#define CMD_LED_SET_ROW                     0x40
