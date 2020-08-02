fb54661229be20894ff5ce90bf4c7cbc0b9b60bc
Steven Impens <s.impens.dev@gmail.com>
2020-08-02 20:05:12 +0200

    Added brightness commands

--- keyboards/annepro2/qmk_ap2_led.h
+++ keyboards/annepro2/qmk_ap2_led.h
@@ -2,5 +2,7 @@
 
 #define CMD_LED_ON                          0x10
 #define CMD_LED_OFF                         0x20
-#define CMD_LED_SET                         0x30
-#define CMD_LED_SET_ROW                     0x40
+#define CMD_LED_BRT_UP                      0x30
+#define CMD_LED_BRT_DOWN                    0x40
+#define CMD_LED_SET                         0x50
+#define CMD_LED_SET_ROW                     0x60
